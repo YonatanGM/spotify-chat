3be47fe932e7b4a5669a63591b4cca09c2843ac9
Yonatan <yonatan@docsy.de>
2022-05-07 12:36:42 +0200

    handle messages and conversation

--- Models/Message.swift
+++ Models/Message.swift
@@ -7,6 +7,8 @@
 
 import MessageKit
 import Foundation
+import CoreLocation
+import UIKit
 
 struct Message: MessageType {
     var sender: SenderType
@@ -15,10 +17,47 @@ struct Message: MessageType {
     var kind: MessageKind
 }
 
-struct Sender: SenderType {
-    var senderId: String
-    var displayName: String
-    var photoURL: String
+extension MessageKind {
+    var messageKindString: String {
+        switch self {
+        case .text(_):
+            return "text"
+        case .attributedText(_):
+            return "attributed_text"
+        case .photo(_):
+            return "photo"
+        case .video(_):
+            return "video"
+        case .location(_):
+            return "location"
+        case .emoji(_):
+            return "emoji"
+        case .audio(_):
+            return "audio"
+        case .contact(_):
+            return "contact"
+        case .custom(_):
+            return "customc"
+        case .linkPreview(_):
+            return "link"
+        }
+    }
 }
 
+struct Sender: SenderType {
+    public var photoURL: String
+    public var senderId: String
+    public var displayName: String
+}
 
+struct Media: MediaItem {
+    var url: URL?
+    var image: UIImage?
+    var placeholderImage: UIImage
+    var size: CGSize
+}
+
+struct Location: LocationItem {
+    var location: CLLocation
+    var size: CGSize
+}
